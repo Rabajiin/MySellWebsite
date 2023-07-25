2f2fd7d860beda1c33988ff98a201363c1ddc1f8
Silentsupt <rbj2560@gmail.com>
2023-07-26 01:08:17 +0700

    .

---
+++ index.js
@@ -0,0 +1,11 @@
+var product = [{
+    id: 1,
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fpremium&psig=AOvVaw3CPmkjnK-KRaYdIJ5FmQ9X&ust=1690394659059000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOia2_u4qoADFQAAAAAdAAAAABAE',
+    name: 'YouTube',
+    price: 30,
+    description: 'YouTube Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quisquam architecto, repellendus dolore minima obcaecati?',
+    type: 'Premium'
+},{
+    id: 2,
+    img: ''
+},{}]
