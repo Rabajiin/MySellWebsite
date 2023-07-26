ef73dce3b130f89887a89f482eb73f35cad32506
Silentsupt <rbj2560@gmail.com>
2023-07-26 19:01:48 +0700

    .

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 var product = [{
     id: 1,
-    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fpremium&psig=AOvVaw3CPmkjnK-KRaYdIJ5FmQ9X&ust=1690394659059000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOia2_u4qoADFQAAAAAdAAAAABAE',
-    name: 'YouTube',
+    img: 'https://d.line-scdn.net/lcp-prod-photo/20210522_6/1621682035977qsGrs_PNG/5C1A68YUZVRY8DFI6FT6IBFIPJZ7KW.png',
+    name: 'YouTube Premium',
     price: 30,
     description: 'YouTube Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quisquam architecto, repellendus dolore minima obcaecati?',
     type: 'Premium'
 },{
     id: 2,
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Open_book_nae_02.svg/375px-Open_book_nae_02.svg.png',
-    name: 'Book',
-    price: 30,
-    description:'Book Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quisquam architecto, repellendus dolore minima obcaecati?',
-    type: 'Book',
+    img: 'https://media.threatpost.com/wp-content/uploads/sites/103/2021/04/19145523/Discord-Nitro-e1618858537976.png',
+    name: 'Discord Nitro',
+    price: 50,
+    description:'Discord Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quisquam architecto, repellendus dolore minima obcaecati?',
+    type: 'Premium',
 },{
     id:3,
-    img: 'https://assets.adidas.com/images/w_700,h_700,f_auto,q_auto,fl_lossy,c_fill,g_auto/56f2935816e643aab316b398ffd6f8e8_9366/superstar-xlg-shoes.jpg',
-    name: 'Adidas Shoe',
-    price: 250,
+    img: 'logo.jpg',
+    name: 'SilentHub',
+    price: 150,
     description:'Shoe Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quisquam architecto, repellendus dolore minima obcaecati?',
-    type: 'Shoe',
+    type: 'Script',
 }];
+
+
 
 $(document).ready(()=>{
     var html = ''; 
-    //i = 1
-    for (let i = 0; i < 10; i++){
-        //state 3 
+    for (let i = 0; i < product.length; i++){
+        html += `<div class="product-item ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                     <p style="font-size: 1.2vw;" >${product[i].name}</p>
+                     <p style="font-size: 1.2vw;">${numberWithCommas(product[i].price) } THB</p>
+                </div>`;
     }
+    $("#productlist").html(html);
 })
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function search(elem) {
+    console.log(elem.id)
+    var value = $('#'+elem.id).val()
+    console.log(value)
+    var html = ''; 
+
+    for (let i = 0; i < product.length; i++){
+        if( product[i].name.includes(value)){
+        html += `<div class="product-item ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                     <p style="font-size: 1.2vw;" >${product[i].name}</p>
+                     <p style="font-size: 1.2vw;">${numberWithCommas(product[i].price) } THB</p>
+                </div>`;
+        }        
+    }
+    if(html == ''){
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-item").css('display', '')
+}
